de5d4b2d41e452e1b712e943956efb8a13635e34
Tuan An <daomancute@gmail.com>
2025-05-30 16:03:18 +0200

    more test cases for correctness verification

--- opendc-compute/opendc-compute-simulator/src/test/kotlin/org/opendc/compute/simulator/scheduler/DTSchedulerTest.kt
+++ opendc-compute/opendc-compute-simulator/src/test/kotlin/org/opendc/compute/simulator/scheduler/DTSchedulerTest.kt
@@ -15,6 +15,121 @@ import java.time.InstantSource
 
 class DTSchedulerTest {
 
+    @Test
+    fun testBasicScheduling() {
+        val clock = mockk<InstantSource>()
+
+        every { clock.instant() } returns Instant.ofEpochMilli(10)
+
+        val scheduler =
+            DTScheduler(
+                filters = emptyList(),
+                weighers = emptyList(),
+                windowSize = 10,
+                clock = clock,
+                forecast = false,
+            )
+
+        val req = mockk<SchedulingRequest>()
+        every { req.task.flavor.coreCount } returns 2
+        every { req.task.flavor.memorySize } returns 1024
+        every { req.isCancelled } returns false
+        every { req.task.nature } returns TaskNature(true)
+        every { req.task.duration } returns Duration.ofMillis(10)
+        every { req.task.deadline } returns 50
+
+        every { req.task.isExecuted } returns false
+        every { req.task.isPaused } returns false
+        every { req.task.isPausable } returns true
+
+        every { req.task.lowerCarbonThreshold = any() } answers {
+            firstArg<Double>()
+        }
+        every { req.task.upperCarbonThreshold = any() } answers {
+            firstArg<Double>()
+        }
+        every { req.task.setExecuted(any()) } answers {
+            firstArg<Boolean>()
+        }
+        every { req.task.setPauseStatus(any()) } answers {
+            firstArg<Boolean>()
+        }
+
+        scheduler.updateCarbonIntensity(160.0)
+        scheduler.updateCarbonIntensity(170.0)
+        scheduler.updateCarbonIntensity(180.0)
+        scheduler.updateCarbonIntensity(190.0)
+        scheduler.updateCarbonIntensity(110.0)
+        scheduler.updateCarbonIntensity(120.0)
+        scheduler.updateCarbonIntensity(130.0)
+        scheduler.updateCarbonIntensity(140.0)
+        scheduler.updateCarbonIntensity(150.0)
+        scheduler.updateCarbonIntensity(100.0)
+
+        //It should return FAILURE because it tries to schedule, but there is no machine
+        assertEquals(SchedulingResultType.FAILURE, scheduler.select(mutableListOf(req).iterator()).resultType)
+    }
+
+    @Test
+    fun testBasicScheduleToNonDeferrableTask() {
+        val clock = mockk<InstantSource>()
+
+        every { clock.instant() } returns Instant.ofEpochMilli(10)
+
+        val scheduler =
+            DTScheduler(
+                filters = emptyList(),
+                weighers = emptyList(),
+                windowSize = 10,
+                clock = clock,
+                forecast = false,
+            )
+
+        val req = mockk<SchedulingRequest>()
+        every { req.task.flavor.coreCount } returns 2
+        every { req.task.flavor.memorySize } returns 1024
+        every { req.isCancelled } returns false
+        every { req.task.nature } returns TaskNature(false)
+        every { req.task.duration } returns Duration.ofMillis(10)
+        every { req.task.deadline } returns 50
+
+        every { req.task.isExecuted } returns false
+        every { req.task.isPaused } returns false
+        every { req.task.isPausable } returns false
+
+        every { req.task.lowerCarbonThreshold = any() } answers {
+            firstArg<Double>()
+        }
+        every { req.task.upperCarbonThreshold = any() } answers {
+            firstArg<Double>()
+        }
+        every { req.task.setExecuted(any()) } answers {
+            firstArg<Boolean>()
+        }
+        every { req.task.setPauseStatus(any()) } answers {
+            firstArg<Boolean>()
+        }
+        every { req.task.setPausable(any()) } answers {
+            firstArg<Boolean>()
+        }
+
+        scheduler.updateCarbonIntensity(110.0)
+        scheduler.updateCarbonIntensity(120.0)
+        scheduler.updateCarbonIntensity(130.0)
+        scheduler.updateCarbonIntensity(140.0)
+        scheduler.updateCarbonIntensity(150.0)
+        scheduler.updateCarbonIntensity(100.0)
+        scheduler.updateCarbonIntensity(160.0)
+        scheduler.updateCarbonIntensity(170.0)
+        scheduler.updateCarbonIntensity(180.0)
+        scheduler.updateCarbonIntensity(190.0)
+
+        //It should return FAILURE because it tries to schedule, but there is no machine
+        assertEquals(SchedulingResultType.FAILURE, scheduler.select(mutableListOf(req).iterator()).resultType)
+    }
+
+
+
 
     @Test
     fun testBasicDeferring() {
